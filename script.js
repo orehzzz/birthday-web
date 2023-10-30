29067586507967684a28ac26ec47dee0a2806246
orehzzz <olegzzz749@gmail.com>
2023-10-30 22:43:44 +0200

    js css html files

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        firstDay: '1',
+        headerToolbar: {
+            right: 'today prev,next'
+        },
+    });
+    calendar.render();
+});
+
+function onTelegramAuth(user) {
+    //make a request to api
+    alert('Logged in as ' + user.first_name);
+}
