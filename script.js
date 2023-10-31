bcb5b5efafbd64905393be7a7d8cd5d53c8fb869
orehzzz <olegzzz749@gmail.com>
2023-10-31 19:08:50 +0200

    wip

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
@@ -12,7 +11,18 @@ document.addEventListener('DOMContentLoaded', function () {
     calendar.render();
 });
 
-function onTelegramAuth(user) {
+async function onTelegramAuth(user) {
     //make a request to api
+    const response = await fetch('http://127.0.0.1:8080/birthdays');
+    const data = await response.json();
+    console.log(data);
+    const date = new Date();
+    for (let i = 0; i < data.length; i++) {
+        calendar.addEvent({
+            title: data['name'],
+            start: `${date.getFullYear()}-${data['month']}-${data['day']}`,
+            allDay: true
+        });
+    }
     alert('Logged in as ' + user.first_name);
 }
