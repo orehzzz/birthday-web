0ac16e2e1dd55a7ca673ab1e91bdbe3b0975988f
orehzzz <olegzzz749@gmail.com>
2023-11-23 17:44:32 +0200

    jwt works 100%

--- script.js
+++ script.js
@@ -14,34 +14,73 @@ function renderCalendar() {
 };
 
 
+// async function apiAuth(user) {
+//     return new Promise(function (resolve, reject) {
+//         fetch_args = {}
+//         for (const [key, val] of Object.entries(user)) {
+//             fetch_args[key] = val;
+//         }
+//         if (fetch('http://127.0.0.1:8080/login?' + new URLSearchParams(fetch_args))) {
+//             resolve();
+//         }
+//         reject();
+//     });
+// }
 async function apiAuth(user) {
-    return new Promise(function (resolve, reject) {
-        fetch_args = {}
-        for (const [key, val] of Object.entries(user)) {
-            fetch_args[key] = val;
-        }
-        fetch('http://127.0.0.1:8080/login?' + new URLSearchParams(fetch_args));
-        resolve();
-    });
+    fetch_args = {}
+    for (const [key, val] of Object.entries(user)) {
+        fetch_args[key] = val;
+    };
+    await fetch('http://127.0.0.1:8080/login?' + new URLSearchParams(fetch_args),
+        { credentials: 'omit' }
+    );
+    console.log("logged in")
+    var theCookies = document.cookie.split(';');
+    var aString = '';
+    for (var i = 0; i <= theCookies.length; i++) {
+        aString += i + ' ' + theCookies[i - 1] + "\n";
+    }
+    console.log(aString)
+    return true
 }
 
 
 function getCookie(name) {
+    console.log('lol')
+    console.log(document.cookie);
     const value = `; ${document.cookie}`;
+    console.log(value)
     const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
+    console.log(parts)
+    console.log(parts.length)
+    if (parts.length === 2) {
+        console.log(name)
+        return parts.pop().split(';').shift();
+    }
 }
 
+// function getCookie(name) {
+//     function escape(s) { return s.replace(/([.*+?\^$(){}|\[\]\/\\])/g, '\\$1'); }
+//     var match = document.cookie.match(RegExp('(?:^|;\\s*)' + escape(name) + '=([^;]*)'));
+//     return match ? match[1] : null;
+// }
 
-async function getData() {
-    console.log("data start")
+
+async function getInitData() {
+    console.log('start get data')
+    console.log(getCookie('csrf_access_token'));
+    setTimeout(function () {
+        console.log(getCookie('csrf_access_token'));
+    }, 2000);
     const response = await fetch('http://127.0.0.1:8080/birthdays',
         {
             headers: { 'X-CSRF-TOKEN': getCookie('csrf_access_token') },
-            method: "GET"
+            method: "GET",
+            credentials: "omit"
         });
+    console.log('made request')
     const data = await response.json();
-    console.log(data)
+    console.log('went through awaits')
     const date = new Date();
     for (let i = 0; i < data.length; i++) {
         let name = data[i]['name'];
@@ -61,7 +100,7 @@ async function getData() {
 
 
 function onTelegramAuth(user) {
-    apiAuth(user).then(function () { getData() })
+    apiAuth(user).then(function () { getInitData() })
     // alert('Logged in as ' + user.first_name);
 }
 
